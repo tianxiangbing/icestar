d5ccecbd5cc3deb9458d7670031e848cc195ae24
tianxiangbing <tianxiangbing@erayt.com>
2018-09-09 17:32:46 +0800

    '2.0.6'

--- server/mockServer.js
+++ server/mockServer.js
@@ -114,10 +114,10 @@ let httpserver = {
     bindConfig() {
         app.all('*', (req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With,titanone-client');
             res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
             if (req.method == 'OPTIONS') {
-                res.send(200); /让options请求快速返回/
+                res.send(204); /让options请求快速返回/
             } else {
                 fs.readFile(this.mockpath, 'utf8', (err, data) => {
                     if (err) {
