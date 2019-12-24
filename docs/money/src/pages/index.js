730f734cff3a6f90292bd0d019c1dc7bb0b06d72
tianxiangbing <55342775@qq.com>
2019-12-24 10:02:34 +0800

    2.1.2

--- docs/money/src/pages/index.js
+++ docs/money/src/pages/index.js
@@ -1,9 +1,16 @@
+/*
+ * @Descripttion: 
+ * @Author: tianxiangbing
+ * @Date: 2018-12-10 09:52:23
+ * @LastEditTime: 2019-11-04 11:15:10
+ * @github: https://github.com/tianxiangbing
+ */
 import styles from './index.css';
 
 export default function() {
   return (
     <div className={styles.normal}>
-      
+      111
     </div>
   );
 }
